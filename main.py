3b438db88f3055dd1815d6359b76c272570e0cac
Pax-Amandi <9334590+Pax-Amandi@users.noreply.github.com>
2018-02-14 11:43:23 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+__author__ = ""
+__copyright__ = ""
+__email__ = ""
+
+
+
+def rot13(sring):
+    """
+    
+    docstring располагается здесь 
+    
+    """
+
+    # ваш код располагается ниже этой строки 
+    pass  # pass можно удалить
+
+    # return
+
+
+if __name__ == '__main__':
+	# тесты располагаются ниже этой строки 
+	pass  # pass можно удалить
